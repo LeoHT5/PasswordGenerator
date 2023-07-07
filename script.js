6e4c0b75c8264fc9311e59f553680acfe07f15a7
Leo <leoste898@gmail.com>
2023-07-06 22:23:25 -0500

    Agregur una nueva funcion llamada generatePassword(), que hace que las opciones funcionen (script.js) - cambio el dato de onchange para llamar a la nueva funcion generatePassword() (index.html)

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+let textCase = '';
 let lowercaseLetters = [];
 let uppercaseLetters = [];
 let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
@@ -42,8 +43,9 @@ function text(x) {
   textoElement.value = x;
 }
 
+let randomTextGeneral = '';
 function addGeneral() {
-  let randomTextGeneral = '';
+  randomTextGeneral = '';
   for (let i = 0; i < 30; i++) {
     const randomType = Math.floor(Math.random() * 4);
     switch (randomType) {
@@ -61,42 +63,156 @@ function addGeneral() {
         break;
     }
   }
-  return text(randomTextGeneral);
+  text(randomTextGeneral);
+}
+
+function generatePassword() {
+  randomTextGeneral = '';
+  let uppercaseCheckbox = document.getElementById('flexSwitchUppercase');
+  let lowercaseCheckbox = document.getElementById('flexSwitchLowercase');
+  let numberCheckbox = document.getElementById('flexSwitchNumbers');
+  let symbolCheckbox = document.getElementById('flexSwitchSymbols');
+
+  if (uppercaseCheckbox.checked && lowercaseCheckbox.checked && numberCheckbox.checked && symbolCheckbox.checked) {
+    for (let i = 0; i < 30; i++) {
+      let random = Math.random();
+      if (random < 0.25) {
+        randomTextGeneral += getRandomUppercaseLetter();
+      } else if (random < 0.5) {
+        randomTextGeneral += getRandomLowercaseLetter();
+      } else if (random < 0.75) {
+        randomTextGeneral += getRandomNumbers();
+      } else {
+        randomTextGeneral += getRandomCharacters();
+      }
+    }
+  } else if (uppercaseCheckbox.checked && lowercaseCheckbox.checked && numberCheckbox.checked) {
+    // Generar combinación de mayúsculas, minúsculas y números
+    for (let i = 0; i < 30; i++) {
+      let random = Math.random();
+      if (random < 0.25) {
+        randomTextGeneral += getRandomUppercaseLetter();
+      } else if (random < 0.5) {
+        randomTextGeneral += getRandomLowercaseLetter();
+      } else{
+        randomTextGeneral += getRandomNumbers();
+      }
+    }
+  } else if (uppercaseCheckbox.checked && lowercaseCheckbox.checked && symbolCheckbox.checked) {
+    // Generar combinación de mayúsculas, minúsculas y símbolos
+    for (let i = 0; i < 30; i++) {
+      let random = Math.random();
+      if (random < 0.25) {
+        randomTextGeneral += getRandomUppercaseLetter();
+      } else if (random < 0.5) {
+        randomTextGeneral += getRandomLowercaseLetter();
+      } else{
+        randomTextGeneral += getRandomCharacters();
+      }
+    }
+  } else if (lowercaseCheckbox.checked && numberCheckbox.checked && symbolCheckbox.checked) {
+    // Generar combinación de minúsculas, números y símbolos
+    for (let i = 0; i < 30; i++) {
+      let random = Math.random();
+      if (random < 0.25) {
+        randomTextGeneral += getRandomLowercaseLetter();
+      } else if (random < 0.5) {
+        randomTextGeneral += getRandomNumbers();
+      } else{
+        randomTextGeneral += getRandomCharacters();
+      }
+    }
+  } else if (uppercaseCheckbox.checked && lowercaseCheckbox.checked) {
+    // Generar combinación de mayúsculas y minúsculas
+    for (let i = 0; i < 30; i++) {
+      let random = Math.random();
+      if (random < 0.5) {
+        randomTextGeneral += getRandomUppercaseLetter();
+      } else {
+        randomTextGeneral += getRandomLowercaseLetter();
+      } 
+    }
+  } else if (uppercaseCheckbox.checked && numberCheckbox.checked) {
+    // Generar combinación de mayúsculas y números
+    for (let i = 0; i < 30; i++) {
+      let random = Math.random();
+      if (random < 0.5) {
+        randomTextGeneral += getRandomUppercaseLetter();
+      } else {
+        randomTextGeneral += getRandomNumbers();
+      } 
+    }
+  } else if (uppercaseCheckbox.checked && symbolCheckbox.checked) {
+    // Generar combinación de mayúsculas y símbolos
+    for (let i = 0; i < 30; i++) {
+      let random = Math.random();
+      if (random < 0.5) {
+        randomTextGeneral += getRandomUppercaseLetter();
+      } else {
+        randomTextGeneral += getRandomCharacters();
+      } 
+    }
+  } else if (lowercaseCheckbox.checked && numberCheckbox.checked) {
+    // Generar combinación de minúsculas y números
+    for (let i = 0; i < 30; i++) {
+      let random = Math.random();
+      if (random < 0.5) {
+        randomTextGeneral += getRandomLowercaseLetter();
+      } else {
+        randomTextGeneral += getRandomNumbers();
+      } 
+    }
+  } else if (lowercaseCheckbox.checked && symbolCheckbox.checked) {
+    // Generar combinación de minúsculas y símbolos
+    for (let i = 0; i < 30; i++) {
+      let random = Math.random();
+      if (random < 0.5) {
+        randomTextGeneral += getRandomLowercaseLetter();
+      } else {
+        randomTextGeneral += getRandomCharacters();
+      } 
+    }
+  } else if (numberCheckbox.checked && symbolCheckbox.checked) {
+    // Generar combinación de números y símbolos
+    for (let i = 0; i < 30; i++) {
+      let random = Math.random();
+      if (random < 0.5) {
+        randomTextGeneral += getRandomNumbers();
+      } else {
+        randomTextGeneral += getRandomCharacters();
+      } 
+    }
+  } else if (uppercaseCheckbox.checked) {
+    // Generar solo mayúsculas
+    for (let i = 0; i < 30; i++) {
+        randomTextGeneral += getRandomUppercaseLetter();
+      }
+  } else if (lowercaseCheckbox.checked) {
+    // Generar solo minúsculas
+    for (let i = 0; i < 30; i++) {
+      randomTextGeneral += getRandomLowercaseLetter();
+    }
+  } else if (numberCheckbox.checked) {
+    // Generar solo números
+    for (let i = 0; i < 30; i++) {
+      randomTextGeneral += getRandomNumbers();
+    }
+  } else if (symbolCheckbox.checked) {
+    // Generar solo símbolos
+    for (let i = 0; i < 30; i++) {
+      randomTextGeneral += getRandomCharacters();
+    }
+  } else {
+    addGeneral();
+  }
+
+  text(randomTextGeneral);
 }
 
 addGeneral();
-rangeInput.addEventListener('click', function() {
-  addGeneral();
+rangeInput.addEventListener('click', function () {
+  generatePassword();
 });
 
-function addUpperCase() {
-  let textAddUpperCase = '';
-  for (let i = 0; i < 30; i++) {
-    textAddUpperCase += getRandomUppercaseLetter();
-  }
-  return text(textAddUpperCase)
-}
-
-function addLoweCase() {
-  let textAddLowerCase = '';
-  for (let i = 0; i < 30; i++) {
-    textAddLowerCase += getRandomLowercaseLetter();
-  }
-  return text(textAddLowerCase)
-}
-
-function addNumbers() {
-  let textAddNumbers = '';
-  for (let i = 0; i < 30; i++) {
-    textAddNumbers += getRandomNumbers();
-  }
-  return text(textAddNumbers)
-}
-
-function addCharacters() {
-  let textAddCharacters = '';
-  for (let i = 0; i < 30; i++) {
-    textAddCharacters += getRandomCharacters();
-  }
-  return text(textAddCharacters)
-}
+//Seleccionar el rango de la contraseña y agregar una opcion que se llame all caracters y siempre este seleccionado cuando desactivamos las demas opciones
+// y se desactive cuando activo alguna otra opcion
